9a15603d3a85857c024ab059bd7aaf5456738e20
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-02-20 19:24:59 -0600

    Primer Commit

---
+++ HolaMundo/HolaMundo/HolaMundo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HolaMundo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void saludar(object sender, EventArgs e)
+        {
+
+            string nm= nmInput.Text;
+            resultado.Text = $"Bienvenido {nm}, saludos!";
+
+        }
+    }
+}
